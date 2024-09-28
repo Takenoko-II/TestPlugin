66bf74c089026807bc8705e00791c8e6497ef64f
たけのこII <yu2.aoki0229.@gmail.com>
2024-09-28 23:53:33 +0900

    鉄剣だけにした

--- src/main/java/com/gmail/subnokoii78/testplugin/events/CustomEventListener.java
+++ src/main/java/com/gmail/subnokoii78/testplugin/events/CustomEventListener.java
@@ -4,7 +4,9 @@ import com.gmail.subnokoii78.testplugin.system.PlayerComboHandler;
 import com.gmail.subnokoii78.util.event.PlayerLeftClickEvent;
 import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.format.NamedTextColor;
-import org.bukkit.entity.Player;
+import org.bukkit.Material;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
 
 public final class CustomEventListener {
     public static final CustomEventListener INSTANCE = new CustomEventListener();
@@ -14,10 +16,15 @@ public final class CustomEventListener {
     public void onLeftClick(PlayerLeftClickEvent event) {
         final PlayerComboHandler handler = PlayerComboHandler.getHandler(event.getPlayer());
 
+        final ItemStack itemStack = event.getPlayer().getEquipment().getItem(EquipmentSlot.HAND);
+
+        if (!itemStack.getType().equals(Material.IRON_SWORD)) return;
+
         if (PlayerLeftClickEvent.Action.ENTITY_HIT.equals(event.getAction())) {
             final Integer currentCount = handler.nextCombo();
             if (currentCount == null) {
                 event.getPlayer().sendMessage(Component.text("CT中").color(NamedTextColor.GRAY));
+                event.cancel();
             }
             else event.getPlayer().sendMessage(Component.text("段数: " + currentCount));
         }
